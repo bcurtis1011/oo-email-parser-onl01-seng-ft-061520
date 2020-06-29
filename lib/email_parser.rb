838a136c9752458df8e9f8046e3531a9180ca5ef
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-29 23:22:46 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,14 +3,3 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser
-  
-  attr_accessor: email
-  
-  def initialize(email)
-    @emails = email
-    
-  end
-  
-  
-end
